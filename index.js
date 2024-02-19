512669002c3261b804c5b89bd94855ba617c288a
eshkarn1 <eshkarnsingh@gmail.com>
2024-02-19 06:43:22 -0500

    added edit button on blogs

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
-const blog = "";
+let blogs = [];
+let lastId = 0;
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -12,16 +14,40 @@ app.get("/", (req, res) => {
     res.render("index.ejs")
 });
 app.get("/add", (req, res) => {
-    res.render("add.ejs", {
+    res.render("add.ejs")
+});
+app.post("/submit", (req,res) => {
+    const blog = { 
+        id: ++lastId,
         title: req.body["title"],
-        blog: req.body["blog"],
+        content: req.body["blog"],
+    };
+    
+    blogs.push(blog);
+    res.redirect("/view")
+});
+app.get("/view", (req,res) => {
+    res.render("view.ejs", {
+        blogs: blogs,
+    });
+});
+app.get("/edit/:id", (req, res) => {
+    const id = parseInt(req.params["id"]);
+    const blog = blogs.find(b => b.id === id);
+    res.render("edit.ejs", {
+        blog: blog,
     })
 });
-app.post("/view", (req,res) => {
-    res.render("view.ejs", {
-        
-    })
-})
+app.post("/edit/:id", (req,res) => {
+    const id = parseInt(req.params["id"]);
+    const index = blogs.findIndex(b => b.id === id);
+    blogs[index] = {
+        id, 
+        title: req.body["title"],
+        content: req.body["blog"],
+    }
+    res.redirect("/view");
+});
 app.listen(port, (req,res) => {
     console.log(`Server is listening on ${port}`)
 });
